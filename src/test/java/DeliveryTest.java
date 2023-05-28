aa5c1c3eaa47c9fb693c69ea2768149b09ca69eb
Neto test <ilmir.kusmaev03@mail.ru>
2023-05-28 12:49:07 +0400

    fix date

--- src/test/java/DeliveryTest.java
+++ src/test/java/DeliveryTest.java
@@ -1,22 +1,31 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
+
 public class DeliveryTest {
+
+    PlanningDate planningDate = new PlanningDate();
+    int days = 4;
+
     @Test
     public void delTest() {
         open("http://localhost:9999/");
         $("[placeholder=Город]").setValue("Уфа");
-        $$("[type=tel]").get(0).setValue("31.05.2023");
+        $$("[type=tel]").get(0).sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $$("[type=tel]").get(0).setValue(planningDate.generateDate(4));
         $("[name=name]").setValue("Иван Иванов");
         $("[name=phone]").setValue("+79372456783");
         $("[class=checkbox__text").click();
         $("[class=button__text]").click();
-        $("[data-test-id=notification]").should(appear, Duration.ofSeconds(12));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate.generateDate(4)), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
